72ca6a59e3152a173a372e507d03a346f35f5fe2
Tony Lau <lht102.p@gmail.com>
2022-04-24 02:34:33 +0800

    Add leetcode problem 2208

---
+++ leetcode/src/02208_minimum_operations_to_halve_array_sum/solution.rs
@@ -0,0 +1,57 @@
+use std::cmp::Ordering;
+use std::collections::BinaryHeap;
+
+#[allow(dead_code)]
+struct Solution;
+
+#[derive(PartialEq)]
+struct NonNan(f64);
+
+impl PartialOrd for NonNan {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.0.partial_cmp(&other.0)
+    }
+}
+
+impl Eq for NonNan {}
+
+impl Ord for NonNan {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.partial_cmp(other).unwrap()
+    }
+}
+
+impl Solution {
+    #[allow(dead_code)]
+    pub fn halve_array(nums: Vec<i32>) -> i32 {
+        let h_sum = nums.iter().map(|&n| n as f64).sum::<f64>() / 2.0;
+        let mut max_heap: BinaryHeap<NonNan> = BinaryHeap::from(
+            nums.into_iter()
+                .map(|n| NonNan(n as f64))
+                .collect::<Vec<NonNan>>(),
+        );
+        let mut res = 0;
+        let mut c_sum = 0.0;
+        while c_sum < h_sum {
+            let v = max_heap.pop().unwrap().0 / 2.0;
+            c_sum += v;
+            max_heap.push(NonNan(v));
+            res += 1;
+        }
+        res
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let nums = vec![5, 19, 8, 1];
+        assert_eq!(Solution::halve_array(nums), 3);
+
+        let nums = vec![3, 8, 20];
+        assert_eq!(Solution::halve_array(nums), 3);
+    }
+}
--- leetcode/src/lib.rs
+++ leetcode/src/lib.rs
@@ -3,3 +3,6 @@ mod two_sum;
 
 #[path = "02244_minimum_rounds_to_complete_all_tasks/solution.rs"]
 mod minimum_rounds_to_complete_all_tasks;
+
+#[path = "02208_minimum_operations_to_halve_array_sum/solution.rs"]
+mod minimum_operations_to_halve_array_sum;
