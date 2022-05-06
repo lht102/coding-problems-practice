4349b171f185222cc507158eab1a48b92d04c0ac
Tony Lau <lht102.p@gmail.com>
2022-05-07 01:24:17 +0800

    Add leetcode problem 1209

---
+++ leetcode/src/01209_remove_all_adjacent_duplicates_in_string_ii/solution.rs
@@ -0,0 +1,45 @@
+struct Solution;
+
+impl Solution {
+    pub fn remove_duplicates(s: String, k: i32) -> String {
+        let mut st = Vec::<(char, usize)>::new();
+        st.push(('\0', 0));
+        for ch in s.chars() {
+            if let Some(last) = st.last_mut() {
+                if last.0 == ch {
+                    last.1 += 1;
+                    if last.1 == k as usize {
+                        st.pop();
+                    }
+                } else {
+                    st.push((ch, 1));
+                }
+            }
+        }
+        let mut res = String::new();
+        for t in st {
+            res.push_str(&std::iter::repeat(t.0).take(t.1).collect::<String>());
+        }
+        res
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let s = String::from("abcd");
+        let k = 2;
+        assert_eq!(Solution::remove_duplicates(s, k), String::from("abcd"));
+
+        let s = String::from("deeedbbcccbdaa");
+        let k = 3;
+        assert_eq!(Solution::remove_duplicates(s, k), String::from("aa"));
+
+        let s = String::from("pbbcggttciiippooaais");
+        let k = 2;
+        assert_eq!(Solution::remove_duplicates(s, k), String::from("ps"));
+    }
+}
--- leetcode/src/lib.rs
+++ leetcode/src/lib.rs
@@ -89,3 +89,6 @@ mod max_number_of_k_sum_pairs;
 
 #[path = "02135_count_words_obtained_after_adding_a_letter/solution.rs"]
 mod count_words_obtained_after_adding_a_letter;
+
+#[path = "01209_remove_all_adjacent_duplicates_in_string_ii/solution.rs"]
+mod remove_all_adjacent_duplicates_in_string_ii;
