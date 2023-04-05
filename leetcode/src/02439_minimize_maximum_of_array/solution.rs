629ee2267f51b0cddbe6b08a6319da1527b0a775
Tony Lau <lht102.p@gmail.com>
2023-04-05 12:39:35 +0800

    Update leetcode problem 2439

--- leetcode/src/02439_minimize_maximum_of_array/solution.rs
+++ leetcode/src/02439_minimize_maximum_of_array/solution.rs
@@ -2,27 +2,15 @@ struct Solution;
 
 impl Solution {
     pub fn minimize_array_value(nums: Vec<i32>) -> i32 {
-        let (mut lo, mut hi) = (nums[0], *nums.iter().max().unwrap());
-        while lo < hi {
-            let mid = lo + (hi - lo) / 2;
-            if Solution::is_valid(&nums, mid) {
-                hi = mid;
-            } else {
-                lo = mid + 1;
-            }
-        }
-        lo
-    }
-
-    fn is_valid(nums: &[i32], max_num: i32) -> bool {
-        let mut sum: i64 = 0;
-        for (i, &num) in nums.iter().enumerate() {
-            sum += num as i64;
-            if sum > max_num as i64 * (i + 1) as i64 {
-                return false;
-            }
-        }
-        true
+        nums.iter()
+            .scan(0, |acc, &num| {
+                *acc += num as i64;
+                Some(*acc)
+            })
+            .enumerate()
+            .map(|(i, prefix_sum)| (prefix_sum + i as i64) / (i + 1) as i64)
+            .max()
+            .unwrap() as _
     }
 }
 
