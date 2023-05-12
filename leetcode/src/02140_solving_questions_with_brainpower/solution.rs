1ee95e338112bc1b0b3f703c4d72a5ce2d717cca
Tony Lau <lht102.p@gmail.com>
2023-05-12 23:08:17 +0800

    Update leetcode problem 2140

--- leetcode/src/02140_solving_questions_with_brainpower/solution.rs
+++ leetcode/src/02140_solving_questions_with_brainpower/solution.rs
@@ -2,23 +2,23 @@ struct Solution;
 
 impl Solution {
     pub fn most_points(questions: Vec<Vec<i32>>) -> i64 {
-        let mut dp = vec![-1; questions.len() + 1];
+        let mut dp = vec![0; questions.len() + 1];
         Self::solve(&questions, 0, &mut dp);
         dp[0]
     }
 
-    fn solve(questions: &Vec<Vec<i32>>, ci: usize, dp: &mut Vec<i64>) -> i64 {
-        if ci >= questions.len() {
+    fn solve(questions: &Vec<Vec<i32>>, i: usize, dp: &mut [i64]) -> i64 {
+        if i >= questions.len() {
             return 0;
         }
-        if dp[ci] != -1 {
-            return dp[ci];
+        if dp[i] != 0 {
+            return dp[i];
         }
-        let res = Self::solve(questions, ci + questions[ci][1] as usize + 1, dp)
-            + questions[ci][0] as i64;
-        let skip = Self::solve(questions, ci + 1, dp);
-        dp[ci] = res.max(skip);
-        dp[ci]
+        let points_without_skip =
+            Self::solve(questions, i + questions[i][1] as usize + 1, dp) + questions[i][0] as i64;
+        let points_with_skip = Self::solve(questions, i + 1, dp);
+        dp[i] = points_without_skip.max(points_with_skip);
+        dp[i]
     }
 }
 
