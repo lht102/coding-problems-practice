6608b07ab285862a2a9fb4753bce4f8caa2cfb44
Tony Lau <lht102.p@gmail.com>
2023-05-20 17:03:58 +0800

    Update leetcode problem 399

--- leetcode/src/00399_evaluate_division/solution.rs
+++ leetcode/src/00399_evaluate_division/solution.rs
@@ -9,33 +9,32 @@ impl Solution {
         values: Vec<f64>,
         queries: Vec<Vec<String>>,
     ) -> Vec<f64> {
-        let n = equations.len();
-        let mut num_v = 0;
-        let mut s_to_v = HashMap::<String, usize>::new();
+        let mut num_of_nodes = 0;
+        let mut str_to_node_idx = HashMap::<&str, usize>::new();
         for equation in &equations {
-            let from = equation[0].clone();
-            if let Entry::Vacant(e) = s_to_v.entry(from) {
-                e.insert(num_v);
-                num_v += 1;
+            let from = &equation[0];
+            if let Entry::Vacant(e) = str_to_node_idx.entry(from) {
+                e.insert(num_of_nodes);
+                num_of_nodes += 1;
             }
-            let to = equation[1].clone();
-            if let Entry::Vacant(e) = s_to_v.entry(to) {
-                e.insert(num_v);
-                num_v += 1;
+            let to = &equation[1];
+            if let Entry::Vacant(e) = str_to_node_idx.entry(to) {
+                e.insert(num_of_nodes);
+                num_of_nodes += 1;
             }
         }
-        let mut dp = vec![vec![-1.0; num_v]; num_v];
-        for i in 0..n {
-            let from = *s_to_v.get(&equations[i][0]).unwrap();
-            let to = *s_to_v.get(&equations[i][1]).unwrap();
-            dp[from][to] = values[i];
-            dp[to][from] = 1.0 / values[i];
+        let mut dp = vec![vec![-1.0; num_of_nodes]; num_of_nodes];
+        for (equation, &value) in equations.iter().zip(values.iter()) {
+            let from = *str_to_node_idx.get(&equation[0].as_ref()).unwrap();
+            let to = *str_to_node_idx.get(&equation[1].as_ref()).unwrap();
+            dp[from][to] = value;
+            dp[to][from] = 1.0 / value;
             dp[from][from] = 1.0;
             dp[to][to] = 1.0;
         }
-        for k in 0..num_v {
-            for i in 0..num_v {
-                for j in 0..num_v {
+        for k in 0..num_of_nodes {
+            for i in 0..num_of_nodes {
+                for j in 0..num_of_nodes {
                     if dp[i][k] != -1.0 && dp[k][j] != -1.0 {
                         dp[i][j] = dp[i][k] * dp[k][j];
                     }
@@ -43,10 +42,10 @@ impl Solution {
             }
         }
         queries
-            .iter()
+            .into_iter()
             .map(|q| {
-                let from = s_to_v.get(&q[0]);
-                let to = s_to_v.get(&q[1]);
+                let from = str_to_node_idx.get(&q[0].as_ref());
+                let to = str_to_node_idx.get(&q[1].as_ref());
                 if from.is_none() || to.is_none() {
                     return -1.0;
                 }
