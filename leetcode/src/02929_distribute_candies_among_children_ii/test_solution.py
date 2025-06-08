8ccaa2994f8bd7ce6b436c1ba07032783e56b92a
Tony Lau <lht102.p@gmail.com>
2025-06-08 23:18:18 +0800

    Add leetcode problem 2929

---
+++ leetcode/src/02929_distribute_candies_among_children_ii/test_solution.py
@@ -0,0 +1,13 @@
+import unittest
+from .solution import Solution
+
+
+class TestTwoSum(unittest.TestCase):
+    def test_distributeCandies(self):
+        s = Solution()
+        self.assertEqual(s.distributeCandies(5, 2), 3)
+        self.assertEqual(s.distributeCandies(3, 3), 10)
+
+
+if __name__ == "__main__":
+    unittest.main()
