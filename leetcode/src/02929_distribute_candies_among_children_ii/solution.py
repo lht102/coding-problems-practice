eb5043c01f9c7306ce71a5b7b84d2fdbb10a8ce8
Tony Lau <lht102.p@gmail.com>
2025-06-15 22:47:04 +0800

    Refactor leetcode problem 2929

--- leetcode/src/02929_distribute_candies_among_children_ii/solution.py
+++ leetcode/src/02929_distribute_candies_among_children_ii/solution.py
@@ -1,8 +1,6 @@
 class Solution:
     def distributeCandies(self, n: int, limit: int) -> int:
-        acc = 0
-        [
-            acc := acc + max(min(limit, n - i) - max(0, n - i - limit) + 1, 0)
+        return sum(
+            max(min(limit, n - i) - max(0, n - i - limit) + 1, 0)
             for i in range(min(n, limit) + 1)
-        ]
-        return acc
+        )
