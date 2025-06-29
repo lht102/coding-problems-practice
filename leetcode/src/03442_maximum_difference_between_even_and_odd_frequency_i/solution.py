a1288badae2fbd9fb6cb295c3bcd9e26482de313
Tony Lau <lht102.p@gmail.com>
2025-06-29 20:34:44 +0800

    Refactor leetcode problem 3442

--- leetcode/src/03442_maximum_difference_between_even_and_odd_frequency_i/solution.py
+++ leetcode/src/03442_maximum_difference_between_even_and_odd_frequency_i/solution.py
@@ -4,6 +4,6 @@ from collections import Counter
 class Solution:
     def maxDifference(self, s: str) -> int:
         counter = Counter(s)
-        max_odd = max([x for x in counter.values() if x % 2 == 1])
-        min_even = min([x for x in counter.values() if x % 2 == 0])
+        max_odd = max(x for x in counter.values() if x % 2 == 1)
+        min_even = min(x for x in counter.values() if x % 2 == 0)
         return max_odd - min_even
